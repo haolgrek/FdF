f0812a361083cca6c8d63395265ef90fb747c65d
Remi LUDER <rluder@e1r6p11.42.fr>
2016-03-06 00:06:40 +0100

    wtf gnl

--- test.c
+++ test.c
@@ -1,5 +1,7 @@
 #include "mlx.h"
+#include "get_next_line.h"
 #include <stdio.h>
+#include "libft/libft.h"
 
 /*int	my_key_funct(int keycode, void *win)
 {
@@ -8,21 +10,40 @@
 	return (0);
 }*/
 
-int	main()
+int	main(int argc, char **argv)
 {
 	void	*mlx;
 	void	*win;
 	void	*img;
+	void	*data;
 	int		x;
 	int		y;
+	char	**tab;
+	char	*line;
+	int		i;
+	int		j;
+	int		fd;
 
-	mlx = mlx_init();
-	win = mlx_new_window(mlx, 400, 400, "mlx 42");
-	img = mlx_new_image(mlx, 200, 200);
-	y = 50;
-	while (y < 150)
+	i = 0;
+	fd = open(argv[1], O_RDONLY);
+	while (get_next_line(fd, &line) != 0)
 	{
-		x = 50;
+		tab[i] = ft_strcpy(tab[i], line);
+		i++;
+	}
+	i = 0;
+	printf("line 0 = %s\n", tab[0]);
+	printf("line 1 = %s\n", tab[1]);
+	printf("line 2 = %s\n", tab[2]);
+	return (0);
+//	mlx = mlx_init();
+//	win = mlx_new_window(mlx, 400, 400, "mlx 42");
+//	img = mlx_new_image(mlx, 200, 200);
+//	data = mlx_get_data_addr(img, 8, , 0);
+//	y = 40;
+/*	while (y < 150)
+	{
+		x = 40;
 		while (x < 150)
 		{
 			mlx_pixel_put(mlx, win, x, y, 0x00FFFFFF);
@@ -31,6 +52,6 @@ int	main()
 		y++;
 	}
 //	mlx_key_hook(win, my_key_funct, win);
-//	mlx_put_image_to_window(mlx, win, img, 50, 50);
-	mlx_loop(mlx);
+	mlx_put_image_to_window(mlx, win, img, 50, 50);
+	mlx_loop(mlx);*/
 }
